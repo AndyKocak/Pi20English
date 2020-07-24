7e15ef31e978be245e7987843e0cb86ef35179ba
Andy Emre Kocak <55792307+AndyEmreHis@users.noreply.github.com>
2020-07-24 22:37:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,13 @@ from setuptools import setup
 import subprocess
 import os
 
-os.system("echo hello")
-os.system('sudo apt-get install libcblas-dev')
-os.system('sudo apt-get install libhdf5-dev')
-os.system('sudo apt-get install libhdf5-serial-dev')
-os.system('sudo apt-get install libqtwebkit4')
-os.system('sudo apt-get install libqt4-test')
-os.system('sudo apt-get install libatlas-base-dev')
-os.system('sudo apt-get install libjasper-dev')
-
 setup(
     name = "Pi20English",
     version = "1.0",
     author = "Andy Emre Kocak, Rana Taki, Yoel Kastro, Yasar Idikut",
     author_email = "emre.kocak@hisarschool.k12.tr, rana.taki@hisarschool.k12.tr, sarp.kastro@hisarschool.k12.tr, yasar.idikut@hisarschool.k12.tr",
     description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
-    packages = ["Pi20"],
+    packages = ["Pi20English"],
     classifiers=["Development Status :: 4 - Beta"],
     install_requires=[
         'picamera',
